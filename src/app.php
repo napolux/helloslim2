8022d5a343fcf9b63972a7a87a157eb190344cec
Napolux <napolux@gmail.com>
2014-08-29 15:29:36 +0200

    Updated to use SlimViews

--- src/app.php
+++ src/app.php
@@ -1,23 +1,36 @@
 <?php
 require_once 'vendor/autoload.php';
 
-// app setup...
-$app    = new \Slim\Slim();
-Twig_Autoloader::register();
-
-// Twig setup...
-$loader = new Twig_Loader_Filesystem('templates');
-$twig   = new Twig_Environment($loader, array(
-
+// Prepare app
+$app = new \Slim\Slim(array(
+    'templates.path' => 'templates',
 ));
 
-// twig globals...
+// Prepare view
+$app->view(new \Slim\Views\Twig());
+
+// Parser options
+$app->view->parserOptions = array(
+    'charset' => 'utf-8',
+    'auto_reload' => true,
+    'strict_variables' => false,
+    'autoescape' => true
+);
+
+// Adding twig extension
+$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
+
+// Twig vars we need in every page...
+$twig = $app->view()->getEnvironment();
+
+// Setting timezone...
+$twig->getExtension('core')->setTimezone('Europe/Rome');
+
 $twig->addGlobal('cssPath', '/assets/css/');
 $twig->addGlobal('jsPath', '/assets/js/');
 
 // Hello World...
-$app->get('/', function () {
-    global $twig;
-    echo $twig->render('pages/homepage.twig', array('pageName' => 'homepage'));
+$app->get('/', function () use ($app) {
+    $app->render('/pages/homepage.twig', array('pageName' => 'homepage'));
 });
 
